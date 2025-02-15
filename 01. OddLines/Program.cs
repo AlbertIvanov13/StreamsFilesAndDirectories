0587bf77d6d22a61af3d6ff270df2ef671ce6d71
Albert Ivanov <albertivanov187@gmail.com>
2025-02-15 13:48:11 +0200

    Add project files.

---
+++ 01. OddLines/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader inputReader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter outputWriter = new StreamWriter(outputFilePath))
+                {
+                    int rowsCount = 0;
+                    while (!inputReader.EndOfStream)
+                    {
+                        string line = inputReader.ReadLine();
+                        if (rowsCount % 2 != 0)
+                        {
+                            outputWriter.WriteLine(line);
+                        }
+                        rowsCount++;
+                    }
+                }
+            }
+        }
+    }
+}
